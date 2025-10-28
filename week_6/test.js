3c422613cfc0c3960873a7dc0c13754c250f94fd
Jack_PC <jfleming48@my.bcit.ca>
2025-10-27 20:40:40 -0700

    updated

--- week_6/test.js
+++ week_6/test.js
@@ -49,4 +49,4 @@ app.get('/dashboard', isLoggedIn, (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'))   
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
